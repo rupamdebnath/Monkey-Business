3652d6e9dedd14dc2951ee92b492f11402019249
Rupam Debnath <44625252+rupamdebnath@users.noreply.github.com>
2022-06-29 16:54:50 +0530

    Completed background spawner script to act as Object Pool, so that ones below can be reused and spawned at top, tested, working correctly

--- MonkeyBusiness/Assets/Scripts/BGSpawner.cs
+++ MonkeyBusiness/Assets/Scripts/BGSpawner.cs
@@ -19,5 +19,35 @@ public class BGSpawner : MonoBehaviour
     {
         height = backgrounds[0].GetComponent<BoxCollider2D>().bounds.size.y;
         highestYPos = backgrounds[0].transform.position.y;
+
+        for(int i = 1; i < backgrounds.Length; i++)
+        {
+            if(backgrounds[i].transform.position.y > highestYPos)
+            {
+                highestYPos = backgrounds[i].transform.position.y;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "BG")
+        {
+            if(target.transform.position.y >= highestYPos)
+            {
+                Vector3 temp = target.transform.position;
+                for(int i = 0; i < backgrounds.Length; i++)
+                {
+                    if(!backgrounds[i].activeInHierarchy)
+                    {
+                        temp.y += height;
+                        backgrounds[i].transform.position = temp;
+                        backgrounds[i].gameObject.SetActive(true);
+
+                        highestYPos = temp.y;
+                    }
+                }
+            }
+        }
     }
 }
