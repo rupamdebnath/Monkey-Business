70e60642407ae76a7fc3662f20af90690a30e17d
Rupam Debnath <44625252+rupamdebnath@users.noreply.github.com>
2022-06-29 17:01:36 +0530

    Added frictionless physics material 2d for player bounds on either sides

--- MonkeyBusiness/Assets/Scripts/Collector.cs
+++ MonkeyBusiness/Assets/Scripts/Collector.cs
@@ -6,7 +6,6 @@ public class Collector : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D target)
     {
-        Debug.Log("Triggered");
         if(target.tag == "BG" || target.tag == "Extrapush" || target.tag == "Normalpush" || target.tag == "Enemy" || target.tag == "Platform")
         {
             target.gameObject.SetActive(false);
